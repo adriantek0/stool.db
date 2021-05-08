b5375e64c584fa6e0351cea244f5bbebf92a4f96
Adrián <79924319+adriantek0@users.noreply.github.com>
2021-05-08 13:50:49 +0200

    🌿 Update

--- src/methods/all.js
+++ src/methods/all.js
@@ -1,17 +1,15 @@
 module.exports = function(db, params, options) {
-  
-  // Fetch Entry
-  var stmt = db.prepare(`SELECT * FROM ${options.table} WHERE ID IS NOT NULL`);
-  let resp = [];
-  for (var row of stmt.iterate()) {
-    try {
-      resp.push({
-        ID: row.ID,
-        data: JSON.parse(row.json)
-      })
-    } catch (e) {}
-  }
-  
-  return resp;
-  
-}
+	const stmt = db.prepare(`SELECT * FROM ${options.table} WHERE ID IS NOT NULL`);
+	const resp = [];
+	for (const row of stmt.iterate()) {
+		try {
+			resp.push({
+				ID: row.ID,
+				data: JSON.parse(row.json),
+			});
+		}
+		catch (e) {}
+	}
+
+	return resp;
+};
